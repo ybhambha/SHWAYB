b02a6ad62a5b1f177983727ed09cedc4b93f2faf
ybhambha <bhambha_yash@hotmail.com>
2016-11-27 08:58:15 -0500

    Restructured SHWAYB folder

--- fstk_utilities_pkg/instruments/rates.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 30 08:04:11 2016
-
-@author: yashwantbhambhani
-"""
-
--- fstk_utilities_pkg/scratchpad/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 31 08:06:49 2016
-
-@author: yashwantbhambhani
-"""
-
